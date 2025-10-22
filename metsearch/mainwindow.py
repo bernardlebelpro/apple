8d8defa965c2007367858bd1a3f63365d7243b8c
bernardlebelpro <152321203+bernardlebelpro@users.noreply.github.com>
2025-10-22 09:35:52 -0700

    UI file

--- metsearch/mainwindow.py
+++ metsearch/mainwindow.py
@@ -3,6 +3,13 @@ from PySide6 import QtCore, QtUiTools, QtWidgets
 import sys
 from typing import Union
 
+for p in sorted(sys.path):
+    print(p)
+
+from .model import ResultsModel, ResultsProxyModel
+
+
+
 
 UI_FILEPATH = os.path.join(os.path.dirname(__file__), "metsearch.ui")
 HEIGHT = 900
@@ -11,12 +18,15 @@ WIDTH = 600
 class MetSearch(QtCore.QObject):
     """The main application object."""
 
-    def __init__(
-            self,
-            parent: Union[QtWidgets.QWidget, None] = None):
+    def __init__(self, parent: Union[QtWidgets.QWidget, None] = None):
         super().__init__(parent)
 
         self._ui: Union[QtWidgets.QMainWindow, None] = None
+        self._model = ResultsModel(parent=self.ui)
+        self._proxy_model = ResultsProxyModel(parent=self.ui)
+        self._proxy_model.setSourceModel(self._model)
+        self._results_view = QtWidgets.QListView(parent=self.ui)
+        self._results_view.setModel(self._proxy_model)
 
         self.setup_ui()
         self.connect_signals()
@@ -24,6 +34,14 @@ class MetSearch(QtCore.QObject):
     # -------------------------------------------------------------------------
     # PROPERTIES
     # -------------------------------------------------------------------------
+
+    @property
+    def model(self) -> ResultsModel:
+        return self._model
+
+    @property
+    def proxy_model(self) -> ResultsProxyModel:
+        return self._proxy_model
 
     @property
     def ui(self) -> QtWidgets.QMainWindow:
@@ -88,7 +106,13 @@ class MetSearch(QtCore.QObject):
 
     @QtCore.Slot()
     def sort_changed(self, *args):
-        pass
+        if self.ui.sortasc_radio.isChecked():
+            order = QtCore.Qt.SortOrder.AscendingOrder
+        else:
+            order = QtCore.Qt.SortOrder.DescendingOrder
+        print(order)
+
+        self.proxy_model.sort(order)
 
     # -------------------------------------------------------------------------
     # METHODS
