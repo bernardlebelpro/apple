0bef88d603ad032751016ce8cbf7f81e894bf826
bernardlebelpro <152321203+bernardlebelpro@users.noreply.github.com>
2025-10-21 21:01:21 -0700

    Add skeleton for MainWindow, update requirements.txt

---
+++ metsearch/metsearch.py
@@ -0,0 +1,105 @@
+import os
+from PySide6 import QtCore, QtUiTools, QtWidgets
+import sys
+from typing import Union
+
+
+UI_FILEPATH = os.path.join(os.path.dirname(__file__), "metsearch.ui")
+HEIGHT = 900
+WIDTH = 600
+
+class MetSearch(QtCore.QObject):
+    """The main application object."""
+
+    def __init__(
+            self,
+            parent: Union[QtWidgets.QWidget, None] = None):
+        super().__init__(parent)
+
+        self._ui: Union[QtWidgets.QMainWindow, None] = None
+
+        self.setup_ui()
+        self.connect_signals()
+
+    # -------------------------------------------------------------------------
+    # PROPERTIES
+    # -------------------------------------------------------------------------
+
+    @property
+    def ui(self) -> QtWidgets.QMainWindow:
+        """Get the main window from Loading the UI file.
+
+        Returns:
+            QtCore.QMainWindow: The main window.
+        """
+        return self._ui
+
+    # -------------------------------------------------------------------------
+    # SETUP METHODS
+    # -------------------------------------------------------------------------
+
+    def connect_signals(self):
+        self.ui.classification_combox.currentTextChanged.connect(
+            self.classification_changed
+        )
+        self.ui.imageonly_checkbox.stateChanged.connect(
+            self.image_only_changed
+        )
+
+        self.ui.searchtext_lineedit.textChanged.connect(
+            self.search_text_changed
+        )
+
+        self.ui.sortasc_radio.toggled.connect(
+            self.sort_changed
+        )
+
+        self.ui.sortdesc_radio.toggled.connect(
+            self.sort_changed
+        )
+
+    def setup_ui(self):
+        loader = QtUiTools.QUiLoader()
+        self._ui = loader.load(UI_FILEPATH, parentWidget=None)
+
+        self.ui.setWindowTitle("MET Search")
+        self.ui.setGeometry(
+            100,
+            100,
+            WIDTH,
+            HEIGHT
+        )  # x, y, width, height
+
+    # -------------------------------------------------------------------------
+    # SLOT METHODS
+    # -------------------------------------------------------------------------
+
+    @QtCore.Slot()
+    def classification_changed(self, new_value: str):
+        pass
+
+    @QtCore.Slot()
+    def image_only_changed(self, new_value: bool):
+        pass
+
+    @QtCore.Slot()
+    def search_text_changed(self, new_value: str):
+        pass
+
+    @QtCore.Slot()
+    def sort_changed(self, *args):
+        pass
+
+    # -------------------------------------------------------------------------
+    # METHODS
+    # -------------------------------------------------------------------------
+
+    def get_classifications(self):
+        pass
+
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    window = MetSearch()
+    window.ui.show()
+    app.exec()
